f053a6f62665870ab9d544e3d95f35eae80ae723
TheCheesyz <55356060+TheCheesyz@users.noreply.github.com>
2019-10-07 13:36:20 -0400

    adjusted size of models via transform
    Should make this adjustable

--- GraphicsAnimation/GraphicsEngine.cpp
+++ GraphicsAnimation/GraphicsEngine.cpp
@@ -135,6 +135,7 @@ void GraphicsEngine::updateUniformBuffer(uint32_t currentImage) {
 
 	UniformBufferObject ubo = {};
 	ubo.model = glm::rotate(glm::mat4(1.0f), time * glm::radians(90.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+	ubo.model = glm::scale(ubo.model, glm::vec3(.1f, .1f, .1f));
 	ubo.view = glm::lookAt(glm::vec3(2.0f, 2.0f, 2.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f));
 	ubo.proj = glm::perspective(glm::radians(45.0f), vh.getSwapchainExtent().width / (float) vh.getSwapchainExtent().height, 0.1f, 10.0f);
 
