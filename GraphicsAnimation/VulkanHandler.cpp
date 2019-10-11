a9e9fa9837ebd2382dce95d5106fc6510b0bc38d
TheCheesyz <55356060+TheCheesyz@users.noreply.github.com>
2019-10-11 14:19:24 -0400

    Small Visual Adjustments

--- GraphicsAnimation/VulkanHandler.cpp
+++ GraphicsAnimation/VulkanHandler.cpp
@@ -72,7 +72,7 @@ void VulkanHandler::createInstance() {
 
 	VkApplicationInfo appInfo = {};
 	appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-	appInfo.pApplicationName = "Triangle";
+	appInfo.pApplicationName = "GEngine";
 	appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
 	appInfo.pEngineName = "No Engine";
 	appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
